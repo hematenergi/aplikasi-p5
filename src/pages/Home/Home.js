8b3baa158671173aa8d1bcc8d6eaf2fae6fccbdb
danyarkham <danyarkham@gmail.com>
2024-07-18 01:42:42 +0700

    add bg overlay

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,21 +5,22 @@ import { useNavigate } from "react-router-dom"
 export const Home = () => {
   const navigate = useNavigate()
   return (
-    <div className="text-center flex flex-col items-center justify-center h-[calc(100vh-300px)]">
-      <div>
-        <h1 className="text-3xl  md:text-6xl lg:text-7xl font-medium">
-          <span className="text-mainColor">Aplikasi form</span> Kegiatan P5
+    <div className="text-center flex flex-col items-center justify-center h-[calc(100vh-200px)]">
+      <div className="absolute z-0 bg-image h-screen w-full bg-blend-overlay" />
+      <div className="z-10">
+        <h1 className="text-3xl md:text-6xl lg:text-7xl font-medium text-white/75">
+          <span className="text-footerColor/75">Aplikasi form</span> Kegiatan P5
         </h1>
       </div>
-      <div data-aos="fade-right" className="text-center margin">
+      <div className="text-center margin z-10">
         <br />
-        <p className="text-sm text-black/75 md:text-md lg:text-lg">
+        <p className="text-sm text-white/75 md:text-md lg:text-lg">
           Klik tombol dibawah ini untuk melakukan pengisian
         </p>
         <br />
       </div>
 
-      <div className="grid grid-rows-1 md:grid-cols-3 lg:grid-cols-3 gap-5">
+      <div className="grid grid-rows-1 md:grid-cols-3 lg:grid-cols-3 gap-5 z-10">
         <Button label={"Form Siswa"} onClick={() => navigate("siswa")} />
         <Button
           label={"Form Kelompok"}
