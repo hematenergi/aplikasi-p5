e7dd747f9ab5d0e324a0d36c4eff998a2544d881
Athoul Muwafiq <athoulmuwafiq@gmail.com>
2024-08-29 20:01:27 +0700

    update url.js

--- src/constant/url.js
+++ src/constant/url.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://127.0.0.1:8000/api";
-// export const baseUrl = "https://aplikasi-p5.site/api";
+// export const baseUrl = "http://127.0.0.1:8000/api";
+export const baseUrl = "https://aplikasi-p5.site/api";
